7927f9ceb7852759fca90a40f49328edf757208f
ts-klassen <git@su-shiki.com>
2025-05-02 00:31:01 +0900

    feat(rest): add symbol/2 endpoint

--- apps/kabue/src/mufje/kabue_mufje_rest_apic.erl
+++ apps/kabue/src/mufje/kabue_mufje_rest_apic.erl
@@ -19,6 +19,7 @@
       , register/2
       , unregister/2
       , unregister_all/1
+      , symbol/2
     ]).
 
 
@@ -201,6 +202,22 @@ order(ReqPayload0, Options) ->
         {left, Left} ->
             {left, Left}
     end.
+
+
+%% ------------------------------------------------------------------
+%%  Symbol information
+%% ------------------------------------------------------------------
+
+-spec symbol(ticker(), options()) -> either(payload()).
+symbol(#{symbol := SymbolBin, exchange := ExchangeAtom}, Options) ->
+    ExchangeCode = maps:get(ExchangeAtom, kabue_mufje_enum:exchange()),
+    Uri = iolist_to_binary([
+        "/kabusapi/symbol/",
+        SymbolBin,
+        "@",
+        klsn_binstr:from_any(ExchangeCode)
+    ]),
+    request(#{uri => Uri, method => get}, Options).
 
 
 -spec register(
